1daf48778b4377fa585253006b20c05b4471c220
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-06-14 15:28:21 +0200

    created function make_batch in dataManager.jl wrapping the whole batch generation

--- Src/dataManager.jl
+++ Src/dataManager.jl
@@ -10,7 +10,27 @@ using Base.Iterators: repeated, partition
 # targets N_samples x 1
 # bin_targets: N_samples x 10
 
-function make_minibatch(X, Y, idxset, batch_size=128, filepath="../digitclutter/src/light_debris/light_debris_with_debris.mat")
+# SOURCE: https://github.com/FluxML/model-zoo/blob/master/vision/mnist/conv.jl
+# Bundle images together with labels into batches 
+# X should be of size Width x Height x 1 x Samples
+# Y should be of size 10 x Samples (encoded as binary targets)
+# X_batch is 32 x 32 x 1 x batchsize
+# Y_batch is 10 x batchsize
+function make_minibatch(X, Y, idxset)
+    X_batch = Array{Float32}(undef, size(X, 1), size(X, 2), 1, length(idxset))
+    Y_batch = Array{Float32}(undef, 10, length(idxset))
+    for i in 1:length(idxset)
+        
+        X_batch[:, :, :, i] = Float32.(X[:, :, :, idxset[i]])
+        Y_batch[:, i] = Y[:, idxset[i]]
+    end    
+    # train_set is a set of tuples (x_batch, y_batch) - first bracket -> which tupel, second bracket -Y what of the tupel
+    # train_set[1][1]
+
+    return (X_batch, Y_batch)
+end # function make_minibatch
+
+function make_batch(batch_size=128, filepath="../digitclutter/src/light_debris/light_debris_with_debris.mat")
     # load the data from the mat file
     file = matopen("../digitclutter/src/light_debris/light_debris_with_debris.mat")
     images = read(file, "images")
@@ -26,25 +46,9 @@ function make_minibatch(X, Y, idxset, batch_size=128, filepath="../digitclutter/
     # uncomment to display one sample of the images
     # matshow(dropdims(images[:,:,:,10], dims=3), cmap=PyPlot.cm.gray, vmin=0, vmax=255)#, cmap = gray, vmin=0, vmax=255)
 
-    # SOURCE: https://github.com/FluxML/model-zoo/blob/master/vision/mnist/conv.jl
-    
-    # Bundle images together with labels into batches 
-    # X should be of size Width x Height x 1 x Samples
-    # Y should be of size 10 x Samples (encoded as binary targets)
-    # X_batch is 32 x 32 x 1 x batchsize
-    # Y_batch is 10 x batchsize
     idxsets = partition(1:size(images, 4), batch_size)
     train_set = [make_minibatch(images, bin_targets, i) for i in idxsets];
-    X_batch = Array{Float32}(undef, size(X, 1), size(X, 2), 1, length(idxset))
-    Y_batch = Array{Float32}(undef, 10, length(idxset))
-    for i in 1:length(idxset)
-        
-        X_batch[:, :, :, i] = Float32.(X[:, :, :, idxset[i]])
-        Y_batch[:, i] = Y[:, idxset[i]]
-    end    
-    # train_set is a set of tuples (x_batch, y_batch) - first bracket -> which tupel, second bracket -Y what of the tupel
-    # train_set[1][1]
-
-    return (X_batch, Y_batch)
-end # function make_minibatch
+    
+    return train_set
+end # function make_batch
 end # module dataManager
