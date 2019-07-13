d48960c533f5339bd5dfc9e3694db49edc3f94e8
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-07-13 18:47:56 +0200

    added printouts to the evaluator.jl and changed to absolute paths

--- Src/evaluator.jl
+++ Src/evaluator.jl
@@ -28,17 +28,19 @@ function load_model(model_cfg::String, data_cfg::String)
 	return model
 end
 load_model(model_cfg::Tuple{String, Int64}) = return [load_model(model_cfg[1], dataset_cfg) for (dataset_cfg, i) in dataset_config]
+@info("loading models")
 models = [load_model(model_cfg) for model_cfg in model_config]
 
 
 
 # load datasets
-test_set_10debris, tmp1, tmp2 = make_batch("../digitclutter/digitdebris/testset/mat/", ["5000_10debris1.mat", "5000_10debris2.mat"]..., batch_size=batch_size)
-test_set_30debris, tmp1, tmp2 = make_batch("../digitclutter/digitdebris/testset/mat/", ["5000_30debris1.mat", "5000_30debris2.mat"]..., batch_size=batch_size)
-test_set_50debris, tmp1, tmp2 = make_batch("../digitclutter/digitdebris/testset/mat/", ["5000_50debris1.mat", "5000_50debris2.mat"]..., batch_size=batch_size)
-test_set_3digits, tmp1, tmp2 = make_batch("../digitclutter/digitclutter/testset/mat/", ["5000_3digits1.mat", "5000_3digits2.mat"]..., batch_size=batch_size)
-test_set_4digits, tmp1, tmp2 = make_batch("../digitclutter/digitclutter/testset/mat/", ["5000_4digits1.mat", "5000_4digits2.mat"]..., batch_size=batch_size)
-test_set_5digits, tmp1, tmp2 = make_batch("../digitclutter/digitclutter/testset/mat/", ["5000_5digits1.mat", "5000_5digits2.mat"]..., batch_size=batch_size)
+@info("loading datasets")
+test_set_10debris, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitdebris/testset/mat/", ["5000_10debris1.mat", "5000_10debris2.mat"]..., batch_size=batch_size)
+test_set_30debris, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitdebris/testset/mat/", ["5000_30debris1.mat", "5000_30debris2.mat"]..., batch_size=batch_size)
+test_set_50debris, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitdebris/testset/mat/", ["5000_50debris1.mat", "5000_50debris2.mat"]..., batch_size=batch_size)
+test_set_3digits, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitclutter/testset/mat/", ["5000_3digits1.mat", "5000_3digits2.mat"]..., batch_size=batch_size)
+test_set_4digits, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitclutter/testset/mat/", ["5000_4digits1.mat", "5000_4digits2.mat"]..., batch_size=batch_size)
+test_set_5digits, tmp1, tmp2 = make_batch("/home/svendt/NNFeedbackOperations/digitclutter/digitclutter/testset/mat/", ["5000_5digits1.mat", "5000_5digits2.mat"]..., batch_size=batch_size)
 datasets = [test_set_10debris, test_set_30debris, test_set_50debris, test_set_3digits, test_set_4digits, test_set_5digits]
 
 # generate model outputs
@@ -70,6 +72,7 @@ get_modeloutput(model::Array{T, 1}, datasets::Array{Array{Tuple{Tuple{Array{Floa
 
 #get_modeloutput(models::Array{Array{T,1}, 1}, datasets::Array{Array{Tuple{Tuple{Array{Float32, 2}, Array{Float32, 4}}, 1}, 1}) where T <: Any = 
 #	return [get_modeloutput(models[model_cfg[2]], datasets, model_cfg) for model_cfg in model_config]
+@info("retrieving model outputs")
 model_outputs = [get_modeloutput(models[model_cfg[2]], datasets, model_cfg) for model_cfg in model_config]
 # generate model outputs
 # BModel_output = [get_FF_modeloutput(BModel[i], datasets[i]) for (cfg, i) in dataset_config]
@@ -138,6 +141,7 @@ end
 
 
 # run pairwise McNemar tests
+@info("run pairwise McNemar tests")
 for (data_cfg, i) in dataset_config
 	for (modelA, modelB) in pairwise_tests
 		fields = pairwise_McNemar(model_outputs[modelA][i], model_outputs[modelB][i], datasets[i], data_cfg)
