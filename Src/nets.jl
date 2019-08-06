59bfad90ecaeffc1a83c05794d97f2256b24aa91
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-08-06 10:28:46 +0200

    fixed bug in folderpaths changed output consistently to printf

--- Src/nets.jl
+++ Src/nets.jl
@@ -140,10 +140,10 @@ if(config == "10debris" || config == "30debris" || config == "50debris")
     train_folderpath = train_folderpath_debris
     test_folderpath = test_folderpath_debris
 elseif(config == "3digits" || config == "4digits" || config == "5digits")
-    train_folderpath = train_folderpath_debris
-    test_folderpath = test_folderpath_debris
+    train_folderpath = train_folderpath_digits
+    test_folderpath = test_folderpath_digits
 else
-    @warn("Ups, somehting in the config went wrong...")
+    @printf("Ups, somehting in the config went wrong...")
 end
 
 train_set, mean_img, std_img = make_batch(train_folderpath, train_filenames..., batch_size=batch_size)
@@ -159,26 +159,26 @@ end
 @printf("loaded %d batches of size %d for training\n", length(train_set), size(train_set[1][1], 4))
 @printf("loaded %d batches of size %d for testing\n", length(test_set), size(test_set[1][1], 4))
 
-@info("Training BModel with $config\n")
+@printf("Training BModel with $config\n")
 best_acc = trainFeedforwardNet(BModel, train_set, test_set, "BModel")
 BSON.@save "BModel_$config.bson" BModel best_acc
 
-@info("Training BKModel with $config\n")
+@printf("Training BKModel with $config\n")
 best_acc = trainFeedforwardNet(BKModel, train_set, test_set, "BKModel")
 BSON.@save "BKModel_$config.bson" BKModel best_acc
 
-@info("Training BFModel with $config\n")
+@printf("Training BFModel with $config\n")
 best_acc = trainFeedforwardNet(BFModel, train_set, test_set, "BFModel")
 BSON.@save "BFModel_$config.bson" BFModel best_acc
 
-@info("Training BLModel with $config\n")
+@printf("Training BLModel with $config\n")
 best_acc = trainReccurentNet(BLModel, train_set, test_set, "BLModel")
 BSON.@save "BLModel_$config.bson" BLModel best_acc
 
-@info("Training BTModel with $config\n")
+@printf("Training BTModel with $config\n")
 best_acc = trainReccurentNet(BTModel, train_set, test_set, "BTModel")
 BSON.@save "BTModel_$config.bson" BTModel best_acc
 
-@info("Training BLTModel with $config\n")
+@printf("Training BLTModel with $config\n")
 best_acc = trainReccurentNet(BLTModel, train_set, test_set, "BLTModel")
 BSON.@save "BLTModel_$config.bson" BLTModel best_acc
