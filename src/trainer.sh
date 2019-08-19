97ca38499211f5e979024a486e8a15ab5270d4b9
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-08-19 16:09:03 +0200

    modified trainer.sh for validation

--- src/trainer.sh
+++ src/trainer.sh
@@ -4,12 +4,15 @@ JULIA_DEBUG=Main
 export CUDA_VISIBLE_DEVICES
 export JULIA_DEBUG
 
-julia nets03.jl
-julia nets01.jl
-julia nets003.jl
-julia nets001.jl
-julia nets0002.jl
-julia nets0001.jl
-julia nets00002.jl
-julia nets00001.jl
+epochs=35
+dataset="MNIST"
+
+julia nets.jl 0.03 $epochs $dataset "" BLModel
+julia nets.jl 0.01 $epochs $dataset "" BLModel
+julia nets.jl 0.002 $epochs $dataset "" BLModel
+julia nets.jl 0.03 $epochs $dataset "" BTModel
+julia nets.jl 0.01 $epochs $dataset "" BTModel
+julia nets.jl 0.03 $epochs $dataset "" BLTModel
+julia nets.jl 0.01 $epochs $dataset "" BLTModel
+julia nets.jl 0.002 $epochs $dataset "" BLTModel
 
